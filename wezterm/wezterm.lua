21c076919c83bfa2c152f12337ea3715340e3acf
rai <raiselmartinez@gmail.com>
2026-07-18 20:47:20 -0400

    remove resize from wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -17,6 +17,5 @@ config.font_size = 15.0
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "RESIZE"
 
 return config
